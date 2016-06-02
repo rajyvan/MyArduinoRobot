6e9b1a46f5aa1d6ed74a71a14a5c1e772f14e77a
rajyvan <yvan.rajaonarivony@gmail.com>
2016-06-02 21:05:29 +0200

    Added IR remote

---
+++ IRRemoteDefine.h
@@ -0,0 +1,31 @@
+
+//******************************************************************************
+// IR remote constants define
+//******************************************************************************
+
+#ifndef IRRemoteDefine_h
+#define IRRemoteDefine_h
+
+#define KEY_TOP              0xFF629D
+#define KEY_BOTTOM           0xFFA857
+#define KEY_LEFT             0xFF22DD
+#define KEY_RIGHT            0xFFC23D
+#define KEY_OK               0xFF02FD
+
+#define KEY_1                0xFF6897
+#define KEY_2                0xFF9867
+#define KEY_3                0xFFB04F
+#define KEY_4                0xFF30CF
+#define KEY_5                0xFF18E7
+#define KEY_6                0xFF7A85
+#define KEY_7                0xFF10EF
+#define KEY_8                0xFF38C7
+#define KEY_9                0xFF5AA5
+#define KEY_0                0xFF4AB5
+
+#define KEY_STAR             0xFF42BD
+#define KEY_SHARP            0xFF52AD
+
+
+
+#endif
--- main.ino
+++ main.ino
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+#include <IRremote.h>
+#include "IRRemoteDefine.h"
 
 /**
  * Created by Yvan RAJAONARIVONY
@@ -27,6 +29,15 @@ int rightEnable = 6;
 int rightForward = 7;
 int rightBackward = 5;
 
+int speed = 0;
+
+// IR remote init
+int pin_recept = 9; // On définit le pin 11
+IRrecv ir_recept(pin_recept);
+decode_results ir_decode; // stockage données reçues
+
+boolean test;
+
 void setup() {
 
 // Set motor pins to output
@@ -38,16 +49,154 @@ pinMode(rightEnable, OUTPUT);
 pinMode(rightForward, OUTPUT);
 pinMode(rightBackward, OUTPUT);
 
+disableMotors();
+
+test = true;
+speed = 0;
+
+// Init IR remote pins
+Serial.begin(9600);
+ir_recept.enableIRIn(); // Initialisation de la réception
 }
 
 void loop() {
 
+  if (ir_recept.decode(&ir_decode))
+  {
+      int key = ir_decode.value;
+      Serial.println(ir_decode.value, HEX); // On affiche le code en hexadecimal
+      ir_recept.resume();
+      doCommand(key);
+  }
 
 }
 
+// interpret IR remote command
+void doCommand(int key) {
+  switch (key) {
+    case KEY_OK:
+      brake();
+      Serial.println("Break");
+      break;
+
+    case KEY_1:
+      enableMotors();
+      Serial.println("Motors enabled");
+      break;
+
+    case KEY_0:
+      disableMotors();
+      Serial.println("Motors disabled");
+      break;
+
+    case KEY_TOP:
+      forward(DELTA_TIME);
+      Serial.println("Forward");
+      coast();
+      break;
+
+    case KEY_BOTTOM:
+      backward(DELTA_TIME);
+      Serial.println("Backward");
+      coast();
+      break;
+
+    case KEY_LEFT:
+      turnLeft(DELTA_TIME);
+      Serial.println("Turn left");
+      coast();
+      break;
+
+    case KEY_RIGHT:
+      turnRight(DELTA_TIME);
+      Serial.println("Turn right");
+      coast();
+      break;
+  }
+}
 
 // High level H-bridge commands
 void enableMotors() {
   digitalWrite(leftEnable, HIGH);
   digitalWrite(rightEnable, HIGH);
+}
+
+void disableMotors() {
+  digitalWrite(leftEnable, LOW);
+  digitalWrite(rightEnable, LOW);
+}
+
+void forward(int time) {
+  motorLeftForward();
+  motorRightForward();
+  delay(time);
+}
+
+void coast() {
+  motorLeftCoast();
+  motorRightCoast();
+}
+
+void brake() {
+  motorLeftBrake();
+  motorRightBrake();
+  speed = 0;
+}
+
+void backward(int time) {
+  motorLeftBackward();
+  motorRightBackward();
+  delay(time);
+}
+
+void turnLeft(int time) {
+  motorLeftBackward();
+  motorRightForward();
+  delay(time);
+}
+
+void turnRight(int time) {
+  motorRightBackward();
+  motorLeftForward();
+  delay(time);
+}
+
+void motorLeftForward() {
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftBackward, LOW);
+}
+
+void motorLeftBackward() {
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, HIGH);
+}
+
+void motorLeftCoast() {
+  digitalWrite(leftForward, LOW);
+  digitalWrite(leftBackward, LOW);
+}
+
+void motorLeftBrake() {
+  digitalWrite(leftForward, HIGH);
+  digitalWrite(leftBackward, HIGH);
+}
+
+void motorRightForward() {
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, LOW);
+}
+
+void motorRightBackward() {
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, HIGH);
+}
+
+void motorRightCoast() {
+  digitalWrite(rightForward, LOW);
+  digitalWrite(rightBackward, LOW);
+}
+
+void motorRightBrake() {
+  digitalWrite(rightForward, HIGH);
+  digitalWrite(rightBackward, HIGH);
 }
